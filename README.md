9b2026df7cc8e6ebd838f235aca606e494126088
Alex Kalinins <alex.kalinins@mail.utoronto.ca>
2022-11-23 18:38:08 -0500

    Initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Geometry Processing - Phong Deformation
+
+This is the final project for CSC419 - Geometry Processing; a libigl style implementation of Phong Deformation Paper.
+
---
+++ src/phong_deformation.cpp
@@ -0,0 +1,10 @@
+#include "phong_deformation.h"
+
+void phong_deformation(
+    const Eigen::Vector3d &V,
+    const Eigen::RowVectorXi &e,
+    const Eigen::MatrixXd &X,
+    Eigen::Vector3d &x)
+{
+  x = V;
+}
