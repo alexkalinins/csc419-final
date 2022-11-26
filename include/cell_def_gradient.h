37472c67fa98697b97c3f09c31224e3a76c58ac0
Alex Kalinins <alex.kalinins@mail.utoronto.ca>
2022-11-26 15:06:19 -0500

    Define cell_def_gradient

---
+++ include/cell_def_gradient.h
@@ -0,0 +1,21 @@
+#ifndef CELL_DEF_GRAD_H
+#define CELL_DEF_GRAD_H
+
+#include <Eigen/Core>
+#include <vector>
+
+/**
+ * Computes the cell deformation gradients for a deformed tetrahedral mesh.
+ *
+ * @param V_undef #V by 3 deformed mesh geometry
+ * @param V_def #V by 3 undeformed mesh geometry
+ * @param F #F by 4 tetrahedral mesh topology
+ * @param grad OUTPUT: #F long list of 3 by 3 cell deformation gradients; one for each face in F
+ */
+void cell_def_gradient(
+    const Eigen::MatrixXd &V_undef,
+    const Eigen::MatrixXd &V_def,
+    const Eigen::MatrixXi &F,
+    std::vector<Eigen::Matrix3d> &grad);
+
+#endif
---
+++ src/cell_def_gradient.cpp
@@ -0,0 +1,32 @@
+#include <Eigen/Core>
+#include <vector>
+
+Eigen::Matrix3d edge_basis(const Eigen::MatrixXd &V, const Eigen::RowVector3d e)
+{
+  return V.row(e(1)) - V.row(e(0)), V.row(e(2)) - V.row(e(0)), V.row(e(3)) - V.row(e(0)); // todo T?
+}
+
+void cell_def_gradient(
+    const Eigen::MatrixXd &V_undef,
+    const Eigen::MatrixXd &V_def,
+    const Eigen::MatrixXi &F,
+    std::vector<Eigen::Matrix3d> &grad)
+{
+  grad.resize(F.rows());
+
+  // tet face:
+  Eigen::RowVector3d e;
+
+  // deformed, undeformed edge basis matrices
+  Eigen::Matrix3d mat_def, mat_undef;
+
+  for (int i = 0; i < F.rows(); i++)
+  {
+    e = F.row(i);
+    mat_undef = edge_basis(V_undef, e);
+    mat_def = edge_basis(V_def, e);
+
+    Eigen::Matrix3d F = mat_def * mat_undef.inverse();
+    grad.push_back(F);
+  }
+}
